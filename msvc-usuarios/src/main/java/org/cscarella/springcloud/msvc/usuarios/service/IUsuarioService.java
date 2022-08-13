2336aabda042d8639392249f94c42e4a9e5eaf8a
Cristian Scarella <cristianscarella09@hotmail.com>
2022-08-13 17:06:15 -0300

    Agrego el microservicio usuarios

---
+++ msvc-usuarios/src/main/java/org/cscarella/springcloud/msvc/usuarios/service/IUsuarioService.java
@@ -0,0 +1,18 @@
+package org.cscarella.springcloud.msvc.usuarios.service;
+
+import org.cscarella.springcloud.msvc.usuarios.models.entity.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IUsuarioService {
+    List<Usuario> listar();
+    Optional<Usuario> porId(Long id);
+    Usuario guardar(Usuario usuario);
+    void eliminar(Long id);
+    List<Usuario> listarPorIds(Iterable<Long> ids);
+
+    Optional<Usuario> porEmail(String email);
+
+    boolean existePorEmail(String email);
+}
